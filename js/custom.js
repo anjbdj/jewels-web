b62f721f0d327d9135dae2ab2219f2d76d89162a
anjbdj <anjbdj1@gmail.com>
2023-11-25 14:24:47 +0530

    fetching the gold rate using the js

--- js/custom.js
+++ js/custom.js
@@ -8,28 +8,4 @@ jQuery(function($){
 
 
 
- window.addEventListener("load", (event) => {
-  console.log("page is fully loaded");
-
-
-    var queryURL = "./resources/rates.json";
-
-    fetch(queryURL)
-            .then(function (response) {
-                // response.json() returns a json string,
-                // returning it will convert it 
-                // to a pure JavaScript 
-                // object for the next then's callback
-                return response.json();
-            })
-            .then(function (users) {
-                // users is a JavaScript object here
-                displayUsersAsATable(users);
-            })
-            .catch(function (error) {
-                console.log('Error during fetch: ' + error.message);
-            });
-
-
-});
 
